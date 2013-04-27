e12de7bfac69a582d53f268e09724e71e841d48f
Daniel Holz <dgholz@gmail.com>
2013-04-28 00:20:24 +0100

    command-line classes get a constructed main module object as $self->embra

--- lib/App/Embra/App/Command.pm
+++ lib/App/Embra/App/Command.pm
@@ -10,29 +10,33 @@ use App::Cmd::Setup -command;
 sub embra {
     my ( $self ) = @_;
     use 5.010;
-    state $embra;
-    return $embra ||= do {
-        require Config::MVP::Reader::Finder;
-        require App::Embra::MVP::Assembler;
-        use Try::Tiny;
-        try {
-            Config::MVP::Reader::Finder->read_config(
-                'embra',
-                {
-                    assembler => App::Embra::MVP::Assembler->new,
-                }
-            );
-        } catch {
-            my $e = $_;
-            try {
-                $e->isa('Config::MVP:Error') and 'package not installed' eq $e->ident;
-            } catch {
-                die $e;
-            };
-            my $package = $_->package;
+    require App::Embra;
+    state $embra = App::Embra->from_config_mvp_sequence( sequence => $self->_create_seq );
+}
 
-            my $bundle = $package =~ /^@/ ? ' bundle' : '';
-            die <<"END_DIE";
+sub _create_seq {
+    my ( $self ) = @_;
+    require Config::MVP::Reader::Finder;
+    require App::Embra::MVP::Assembler;
+    use Try::Tiny;
+    try {
+        Config::MVP::Reader::Finder->read_config(
+            'embra',
+            {
+                assembler => App::Embra::MVP::Assembler->new,
+            }
+        );
+    } catch {
+        my $e = $_;
+        try {
+            $e->isa('Config::MVP:Error') and 'package not installed' eq $e->ident;
+        } catch {
+            die $e;
+        };
+        my $package = $_->package;
+
+        my $bundle = $package =~ /^@/ ? ' bundle' : '';
+        die <<"END_DIE";
 Required plugin$bundle [$package] isn't installed.
 
 Run 'embra listdeps' to see a list of all required plugins.
@@ -41,7 +45,6 @@ You can pipe the list to your CPAN client to install or update them:
     embra listdeps | cpanm
 
 END_DIE
-        };
     };
 }
 
