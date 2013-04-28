bb098edfe76dc9c7d2e32a5f4c25f81c6913bdbb
Daniel Holz <dgholz@gmail.com>
2013-04-28 23:14:17 +0100

    plugins self-register with the main module

--- lib/App/Embra.pm
+++ lib/App/Embra.pm
@@ -8,14 +8,27 @@ package App::Embra;
 use Moo;
 use Method::Signatures;
 
+has 'plugins' => (
+    is => 'ro',
+    default => sub{[]},
+);
+
 method from_config_mvp_sequence( $class:, Config::MVP::Sequence :$sequence ) {
     my $root_section = $sequence->section_named( '_' );
     my $creatura = $class->new( $root_section->payload );
     for my $plugin_section ( $sequence->sections ) {
         next if $plugin_section == $root_section;
-        my $plugin = $plugin_section->package->new( $plugin_section->payload );
-        $creatura->add_plugin( $plugin_section->name, $plugin );
+        $plugin_section->package->register_plugin(
+            name => $plugin_section->name,
+            args => $plugin_section->payload,
+            embra => $creatura,
+        );
     }
+    $creatura;
+}
+
+method add_plugin( $plugin where { $_->DOES( "App::Embra::Role::Plugin" ) } ) {
+    push @{ $self->plugins}, $plugin;
 }
 
 1;
