4b3572bd47483a0d20c1808e8613d562ce1c4e20
Daniel Holz <dgholz@gmail.com>
2013-05-22 00:13:19 +0100

    new attr Role::Plugin->name

--- lib/App/Embra/Role/Plugin.pm
+++ lib/App/Embra/Role/Plugin.pm
@@ -13,8 +13,13 @@ has 'embra' => (
     required => 1,
 );
 
+has 'name' => (
+    is => 'ro',
+    default => sub { __PACKAGE__ },
+);
+
 method register_plugin( $class:, :$name, HashRef :$args, App::Embra :$embra ) {
-    my $self = $class->new( embra => $embra, %{ $args } );
+    my $self = $class->new( embra => $embra, name => $name, %{ $args } );
     $embra->add_plugin( $self );
 }
 
