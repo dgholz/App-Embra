08259cdeb09b1f13e29318cf3061258d5a51eb01
Daniel Holz <dgholz@gmail.com>
2013-05-21 23:18:11 +0100

    logging role

---
+++ lib/App/Embra/Role/Logging.pm
@@ -0,0 +1,33 @@
+use strict;
+use warnings;
+
+package App::Embra::Role::Logging;
+
+# ABSTRACT: adds logging methods to a class
+
+use Moo::Role;
+use Method::Signatures;
+use Log::Any;
+
+has 'log_prefix' => (
+    is => 'lazy',
+);
+
+method _build_log_prefix { 
+    return q{};
+}
+
+has 'logger' => (
+    is => 'lazy',
+    handles => [ Log::Any->logging_methods ],
+);
+
+requires '_build_logger';
+
+for my $logging_method ( Log::Any->logging_methods ) {
+    around $logging_method => func( $orig, $self, $log_msg ) {
+        $self->logger->$logging_method( $self->log_prefix . $log_msg );
+    };
+}
+
+1;
