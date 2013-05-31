27d28ee99475d52abb758f5b5ddf07b549c888d3
Daniel Holz <dgholz@gmail.com>
2013-06-01 00:08:46 +0100

    docs for Cname plugin

--- lib/App/Embra/Plugin/Cname.pm
+++ lib/App/Embra/Plugin/Cname.pm
@@ -10,9 +10,28 @@ use Method::Signatures;
 
 use App::Embra::File;
 
+=head1 DESCRIPTION
+
+This plugin will add a F<CNAME> file to the site. Use this plugin if you are hosting the site L<on GitHub with a custom domain name|https://help.github.com/articles/setting-up-a-custom-domain-with-pages#setting-the-domain-in-your-repo>.
+
+=cut
+
+=attr domain
+
+The canonical domain name for the site (the host part of its URI). This is required.
+
+=cut
+
 has 'domain' => (
     is => 'ro',
+    required => 1,
 );
+
+=attr filename
+
+The filename to store the site's canonical domain name in. It defaults to F<CNAME>.
+
+=cut
 
 has 'filename' => (
     is => 'ro',
