628a0c6c87e00d2a41d7fd911f772498d594a34e
Daniel Holz <dgholz@gmail.com>
2013-06-06 00:55:08 +0100

    docs for TransformMarkdown plugin

--- lib/App/Embra/Plugin/TransformMarkdown.pm
+++ lib/App/Embra/Plugin/TransformMarkdown.pm
@@ -9,10 +9,28 @@ use Moo;
 use Method::Signatures;
 use File::Basename;
 
+=head1 DESCRIPTION
+
+This plugin will transform the content of Markdown files into HTML and change the file's extension to C<.html>.
+
+=cut
+
+=attr extension
+
+Files ending with this extension will be treated as Markdown files. Defualts to C<.md>.
+
+=cut
+
 has 'extension' => (
     is => 'ro',
     default => sub { 'md' },
 );
+
+=attr converter
+
+The object to use to convert the file content from Markdown to HTML. Defaults to an instance of L<Text::Markdown>.
+
+=cut
 
 has 'converter' => (
     is => 'ro',
