5a35c9bbf05d05814c8a49a1a7f2cd3f58799b07
Daniel Holz <dgholz@gmail.com>
2013-06-06 00:45:10 +0100

    docs for TemplateToolkit plugin

--- lib/App/Embra/Plugin/TemplateToolkit.pm
+++ lib/App/Embra/Plugin/TemplateToolkit.pm
@@ -10,21 +10,53 @@ use Method::Signatures;
 use Template;
 use Path::Class qw<>;
 
+=head1 DESCRIPTION
+
+This plugin will process site files through Template Toolkit. For each file with a C<.html> extension, it will look for a template in the C<include_path> with a matching name and use it to process the contents of the file into a rendered HTML document.
+
+Templates will be passed the file's content and body as variables, as well as each of the file's notes.
+
+=cut
+
+=attr include_path
+
+Where to find templates. Defaults to F<templates> in the current directory.
+
+=cut
+
 has 'include_path' => (
     is => 'ro',
     default => sub { 'templates' },
     coerce => sub { Path::Class::dir( $_[0] ) },
 );
 
+=attr default_template
+
+Template to use if a file doesn't have a matching template. Defaults to F<default.tt>.
+
+=cut
+
 has 'default_template' => (
     is => 'lazy',
     default => method { 'default'. $self->extension },
 );
 
+=attr extension
+
+The extension of template files. Defaults to C<.tt>.
+
+=cut
+
 has 'extension' => (
     is => 'ro',
     default => sub { '.tt' },
 );
+
+=attr renderer
+
+The object used to render files. Defaults to an instance of L<Template Toolkit|Template>.
+
+=cut
 
 has 'renderer' => (
     is => 'lazy',
