85a6f33b0b0ab47f5b7a0f7a4f6e7b1dbaac4a50
Daniel Holz <dgholz@gmail.com>
2013-06-06 23:56:32 +0100

    docs for FilePruner role

--- lib/App/Embra/Role/FilePruner.pm
+++ lib/App/Embra/Role/FilePruner.pm
@@ -9,9 +9,25 @@ use Method::Signatures;
 use Moo::Role;
 use List::MoreUtils qw< indexes >;
 
+=head1 DESCRIPTION
+
+This role should be implemented by any plugin which prevents files from appearing in your site. It provides one method (C<L</prune_files>>), and requires plugins provide an C<exclude_file> method, which accepts a single L<App::Embra::File> object and return true if the file should be removed.
+
+C<prune_files> will be called after all L<FileGatherer|App::Embra::Role::FileGatherer>-type plugins have added files, and will remove from the site all files which cause C<L</exclude_file>> to return true.
+
+=cut
+
 with 'App::Embra::Role::Plugin';
 
 requires 'exclude_file';
+
+=method prune_files
+
+    $plugin->prune_files;
+
+Passes each of the site's files to the plugin's C<exclude_file> method, and removes the file from the site if the method returns true.
+
+=cut
 
 method prune_files {
     my $files = $self->embra->files;
