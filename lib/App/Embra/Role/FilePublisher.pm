2939fb839c905c62b178b733c5d50b722edb91f9
Daniel Holz <dgholz@gmail.com>
2013-06-07 01:12:35 +0100

    docs for FilePublisher role

--- lib/App/Embra/Role/FilePublisher.pm
+++ lib/App/Embra/Role/FilePublisher.pm
@@ -8,6 +8,14 @@ package App::Embra::Role::FilePublisher;
 use Method::Signatures;
 use Moo::Role;
 
+=head1 DESCRIPTION
+
+This role should be implemented by any plugin which publishes your site. It requires plugins provide a C<publish_files> method,which will be called as the final step of collating your site.
+
+Plugins which implement this role can access the site's files via the C<L<files|App::Embra/files>> attribute of its C<L<embra|App::Embra::Role::Plugin/embra>> attribute, and should publish all files.
+
+=cut
+
 with 'App::Embra::Role::Plugin';
 
 requires 'publish_files';
