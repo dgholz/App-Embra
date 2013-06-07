0d5b9f7222c7b778324ceb75d8eb10ea444a5169
Daniel Holz <dgholz@gmail.com>
2013-06-07 01:17:07 +0100

    docs for FileRenderer role

--- lib/App/Embra/Role/FileRenderer.pm
+++ lib/App/Embra/Role/FileRenderer.pm
@@ -3,10 +3,18 @@ use warnings;
 
 package App::Embra::Role::FileRenderer;
 
-# ABSTRACT: something that renders file content into HTML
+# ABSTRACT: something that renders file content into its publishable form
 
 use Method::Signatures;
 use Moo::Role;
+
+=head1 DESCRIPTION
+
+This role should be implemented by any plugin which alters a file's content into its final format. It requires plugins provide a C<render_files> method, which will be called late in the process of collating your site.
+
+Plugins which implement this role can access the site's files via the C<L<files|App::Embra/files>> attribute of its C<L<embra|App::Embra::Role::Plugin/embra>> attribute.
+
+=cut
 
 with 'App::Embra::Role::Plugin';
 
