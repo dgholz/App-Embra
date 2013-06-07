ee4c01b2aaab4e720beb951a072bcb68f4e63527
Daniel Holz <dgholz@gmail.com>
2013-06-07 01:23:14 +0100

    docs for FilePublisher role

--- lib/App/Embra/Role/FileTransformer.pm
+++ lib/App/Embra/Role/FileTransformer.pm
@@ -3,10 +3,18 @@ use warnings;
 
 package App::Embra::Role::FileTransformer;
 
-# ABSTRACT: something that transforms files before App::Embra turns them into a site
+# ABSTRACT: something that transforms file content, but not into its publishable form
 
 use Method::Signatures;
 use Moo::Role;
+
+=head1 DESCRIPTION
+
+This role should be implemented by any plugin which alters a file's content. It requires plugins provide a C<transform_files> method, which will be called early in the process of collating your site.
+
+Plugins which implement this role can access the site's files via the C<L<files|App::Embra/files>> attribute of its C<L<embra|App::Embra::Role::Plugin/embra>> attribute.
+
+=cut
 
 with 'App::Embra::Role::Plugin';
 
