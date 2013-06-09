03785af9214a48c8986387d05c1fc86ca6cfd5f2
Daniel Holz <dgholz@gmail.com>
2013-06-09 23:38:35 +0100

    Plugin role sets logger by modifying default from Logging role
    instead of providing its own builder

--- lib/App/Embra/Role/Plugin.pm
+++ lib/App/Embra/Role/Plugin.pm
@@ -23,11 +23,11 @@ method register_plugin( $class:, :$name, HashRef :$args, App::Embra :$embra ) {
     $embra->add_plugin( $self );
 }
 
-method _build_logger {
-    $self->embra->logger;
-}
-
 with 'App::Embra::Role::Logging';
+
+around '_build_logger' => func( $orig, $self ) {
+    $self->embra->logger;
+};
 
 around '_build_log_prefix' => func( $orig, $self ) {
     return "[${ \ $self->name }] ";
