4108c685deaa3e822ef295c6237956f7ac29a478
Daniel Holz <dgholz@gmail.com>
2013-06-10 01:02:53 +0100

    docs for Utils

--- lib/App/Embra/Util.pm
+++ lib/App/Embra/Util.pm
@@ -7,6 +7,16 @@ package App::Embra::Util;
 
 use String::RewritePrefix;
 
+=method expand_config_package_name
+
+    App::Embra::Util->expand_config_package_name( $pkg_name )
+
+Returns C<$pkg_name> rewritten into a package name. If C<$pkg_name> starts with C<=>, it is treated as a package name and returned unmodified. Otherwise, a copy of it is prefixed with C<App::Embra::Plugin> (and then C<Bundle>, if it starts with C<@>) and returned.
+
+Used by l<App::Embra::MVP::Assembler> and L<App::Embra::App::Command::listdeps>, to turn section names in config files into package names.
+
+=cut
+
 sub expand_config_package_name {
     my( $class, $pkg_name ) = @_;
     String::RewritePrefix->rewrite(
