97e02fc6340fe1f9e713966e064065ef8cc21f6d
Daniel Holz <dgholz@gmail.com>
2013-06-12 00:24:41 +0100

    GatherDir ignores dotfiles

--- lib/App/Embra/Plugin/GatherDir.pm
+++ lib/App/Embra/Plugin/GatherDir.pm
@@ -30,9 +30,21 @@ has 'from' => (
     coerce => sub { Path::Class::Dir->new( $_[0] ) },
 );
 
+=attr include_dotfiles
+
+Whether to consider files and directories beginning with C<.> (dot) when gathering files. Defaults to false.
+
+=cut
+
+has 'include_dotfiles' => (
+    is => 'ro',
+    default => sub { !1 },
+);
+
 method gather_files {
     $self->from->recurse( callback => func( $file ) {
         return if $file->is_dir;
+        return if $file->basename =~ m/ \A [.] /xms and not $self->include_dotfiles;
         $self->add_file( App::Embra::File->new( name => $file->stringify ) );
     } );
 }
