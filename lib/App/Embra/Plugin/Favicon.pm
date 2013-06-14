4a22f09b5463f0849d7d4542ea25251d7d505c13
Daniel Holz <dgholz@gmail.com>
2013-06-14 01:57:58 +0100

    add favicon to Snippets->head

---
+++ lib/App/Embra/Plugin/Favicon.pm
@@ -0,0 +1,42 @@
+use strict;
+use warnings;
+
+package App::Embra::Plugin::Favicon;
+
+# ABSTRACT: adds a favicon
+
+use Moo;
+use Method::Signatures;
+
+=head1 DESCRIPTION
+
+This plugin will add the HTML to the site to include a favicon. You must add the file separately, and then pass the filename to this plugin.
+
+=cut
+
+=attr filename
+
+The name of the favicon. Defaults to F<favicon.ico>.
+
+=cut
+
+has 'filename' => (
+    is => 'ro',
+    default => sub{ 'favicon.ico' },
+);
+
+has 'fragment' => (
+    is => 'ro',
+    default => method { qq{<link rel="icon" type="image/x-icon" href="$self->filename">} ),
+    init_arg => undef,
+);
+
+has 'clipboard' => (
+    is => 'ro',
+    default => 'head',
+    init_arg => undef,
+);
+
+with 'App::Embra::Role::Snippet';
+
+1;
--- t/app/embra/role/snippet_with.t
+++ t/app/embra/role/snippet_with.t
@@ -1,7 +1,6 @@
 use strict;
 use warnings;
 
-use lib 't/lib';
 use App::Embra::Role::Snippet;
 
 package Foo;
