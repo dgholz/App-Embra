e5fb50daeb32f31b76001c7e917168ef408230eb
Daniel George Holz <dgholz@gmail.com>
2014-03-24 14:20:38 +0000

    doco typo

--- lib/App/Embra/Util.pm
+++ lib/App/Embra/Util.pm
@@ -13,7 +13,7 @@ use String::RewritePrefix;
 
 Returns C<$pkg_name> rewritten into a package name. If C<$pkg_name> starts with C<=>, it is treated as a package name and returned unmodified. Otherwise, a copy of it is prefixed with C<App::Embra::Plugin> (and then C<Bundle>, if it starts with C<@>) and returned.
 
-Used by l<App::Embra::MVP::Assembler> and L<App::Embra::App::Command::listdeps>, to turn section names in config files into package names.
+Used by L<App::Embra::MVP::Assembler> and L<App::Embra::App::Command::listdeps>, to turn section names in config files into package names.
 
 =cut
 
