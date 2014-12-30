906f5f907cb29b9f506966dc406968f2e1ec1734
Daniel George Holz <dgholz@gmail.com>
2014-12-30 09:45:38 -0800

    map A::E::P::Zilla::WrapLog->proxy_prefix to ->log_prefix

--- lib/App/Embra/Plugin/Zilla/WrapLog.pm
+++ lib/App/Embra/Plugin/Zilla/WrapLog.pm
@@ -22,6 +22,15 @@ has '_wrap_logger' => (
     default => method { $self->logger },
 );
 
+has 'proxy_prefix' => (
+    is => 'ro',
+    required => 1,
+);
+
+around 'log_prefix' => func( $orig, $self ) {
+    $self->proxy_prefix;
+};
+
 while( my ($w, $r) = each %_log_level_map ) {
     around $w => func( $orig, $self, $log_msg ) {
         $self->logger->$r( $self->log_prefix . $log_msg );
