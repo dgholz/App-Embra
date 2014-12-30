19548b643bc866d7407dea57380c7001d87826fa
Daniel George Holz <dgholz@gmail.com>
2014-12-30 09:44:17 -0800

    move the mapping of log methods wrapped in A::E::P::Zilla::WrapLog to a hash

--- lib/App/Embra/Plugin/Zilla/WrapLog.pm
+++ lib/App/Embra/Plugin/Zilla/WrapLog.pm
@@ -10,13 +10,15 @@ use Moo;
 
 with 'App::Embra::Role::Logging';
 
+my %_log_level_map = (
+    log => 'info',
+    log_debug => 'debug',
+    log_fatal => 'fatal',
+);
+
 has '_wrap_logger' => (
     is => 'lazy',
-    handles => {
-        log => 'info',
-        log_debug => 'debug',
-        log_fatal => 'fatal',
-    },
+    handles => \%_log_level_map,
     default => method { $self->logger },
 );
 
