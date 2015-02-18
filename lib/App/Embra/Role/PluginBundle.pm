b69a885e8218d9ffb3a2c1eb16eefa01248b7fa6
Daniel George Holz <dgholz@gmail.com>
2015-02-17 22:12:47 -0800

    A::E::R::PluginBundle passes tests

--- lib/App/Embra/Role/PluginBundle.pm
+++ lib/App/Embra/Role/PluginBundle.pm
@@ -11,34 +11,75 @@ use Moo::Role;
 
 =head1 DESCRIPTION
 
-This role provides a few key methods and attributes which all plugins for App::Embra should implement. It consumes the L<Logging role|App::Embra::Role::Logging>, altering L<C<log prefix>|App::Embra::Role::Logging/log_prefix> to include the plugin's name, and L<C<logger>|App::Embra::Role::Logging/logger> to reuse the C<logger> from the instance of App::Embra being plugged into.
+This role will allow the composer to be used as a L<Bundle|Config::MVP::Assembler::WithBundles>. It requires the composer to provide a C<configure_bundle> method, which will be called once after the constructor.
 
 =cut
 
 with 'App::Embra::Role::Plugin';
 
+=attr bundled_plugins_config
+
+A list of plugins to be included. Each element should be an arrayref of:
+
+    [ $package_name, $plugin_name, [ @plugin_args ] ]
+
+where C<$package_name> is the name of the plugin package, C<$plugin_name> will be used for the name of the plugin, and C<@plaugin_args> will be passed to the plugin's constructor.
+
+The helper method C< L</add_plugin> > provides a flexible way to populate this list.
+
+=cut
+
 has 'bundled_plugins_config' => (
     is => 'ro',
-    default => func { [] },
+    default => method { [] },
 );
 
-method add_plugin($pkg, :$name = $pkg, @payload) {
-    $pkg = expand_config_package_name($pkg);
+=method add_plugin
 
+    $self->add_plugin('Foo');
+    $self->add_plugin('Foo', 'Foo with special name');
+
+    # also recognised name as second parameter
+    $self->add_plugin('Foo', extra => 'args', for => 'Foo ctor');
+    $self->add_plugin('Foo', { or => 'args', as => 'hashref' });
+
+    # won't recognised name as second parameter
+    $self->add_plugin('Foo', { with => 'hashref' }, and => 'named args');
+
+Adds the details of a plugin to be included in the bundle. Call this from your C</configure_bundle> method.
+
+=cut
+
+method add_plugin($pkg, @payload) {
     my %payload;
-    if( 1 == @payload and 'HASH' eq $payload[0]) {
-        %payload = %{ $payload[0] };
+    my $name;
+    if(2 == @payload and 'HASH' eq ref $payload[1]) {
+        $name    = shift @payload;
+        %payload = %{ shift @payload };
+    }
+    if(1 == @payload % 2) {
+        if('HASH' eq ref $payload[0]) {
+            %payload = %{ shift @payload };
+        }
+        else {
+            $name = shift @payload;
+        }
+    }
+    %payload = ( %payload, @payload );
+    if( exists $payload{name} ) {
+        $name = delete $payload{name};
     }
     else {
-        %payload = @payload;
+        $name ||= $pkg;
     }
-    push @{ $self->bundled_plugins }, [ $pkg, $name, \( %payload ) ];
+    $pkg = expand_config_package_name($pkg);
+    push @{ $self->bundled_plugins_config }, [ $pkg, $name, [ %payload ] ];
 }
 
-require 'configure_bundle';
+requires 'configure_bundle';
 
-method BUILD {}
+method BUILD($args) {}
 
-after 'BUILD' => func { $self->configure_bundle };
+after 'BUILD' => method($args) { $self->configure_bundle };
 
 1;
