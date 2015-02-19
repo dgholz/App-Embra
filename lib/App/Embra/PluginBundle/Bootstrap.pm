80d2a85de1666586f35d4116c156a8946a564198
Daniel George Holz <dgholz@gmail.com>
2015-02-18 23:49:44 -0800

    @Bootstrap includes JS

--- lib/App/Embra/PluginBundle/Bootstrap.pm
+++ lib/App/Embra/PluginBundle/Bootstrap.pm
@@ -16,8 +16,18 @@ This bundle will add a precompiled version of L<Bootstrap|http://getbootstrap.co
 =cut
 
 method configure_bundle {
-    $self->add_plugin( 'IncludeStyleSheet', name => 'Bootstrap CSS', src => 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css' );
-    $self->add_plugin( 'IncludeStyleSheet', name => 'Bootstrap theme', src => 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css' );
+    $self->add_plugin( 'IncludeStyleSheet',
+        name => 'Bootstrap CSS',
+        src => 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css',
+    );
+    $self->add_plugin( 'IncludeStyleSheet',
+        name => 'Bootstrap theme',
+        src => 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css',
+    );
+    $self->add_plugin( 'IncludeJavaScript',
+        name => 'Bootstrap JS',
+        src => 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js',
+    );
 }
 
 with 'App::Embra::Role::PluginBundle';
