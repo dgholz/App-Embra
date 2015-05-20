71ca2b61b68320b2881615cf7daea21f00bdc39e
Daniel George Holz <dgholz@gmail.com>
2015-05-20 09:05:33 -0700

    A::E::P::IncludeStyleSheet doc update

--- lib/App/Embra/Plugin/IncludeStyleSheet.pm
+++ lib/App/Embra/Plugin/IncludeStyleSheet.pm
@@ -8,6 +8,17 @@ package App::Embra::Plugin::IncludeStyleSheet;
 use Moo;
 use Method::Signatures;
 
+=head1 SYNOPSIS
+
+    # embra.ini
+    [IncludeStyleSheet]
+    src = css/local_file.css
+
+    [IncludeStyleSheet / Example.com retro theme]
+    src = http://example.com/css/retro.css
+
+=cut
+
 =head1 DESCRIPTION
 
 This plugin will add a L<snippet|App::Embra::Role::Snippet> to your site which links to the CSS file L<C<src>|/src>. If C<src> is a URL, it will be linked to directly; if it is a local file, it will be included in the site.
@@ -22,11 +33,23 @@ Where to find the CSS file. Can be a path or a URL.
 
 # hi I'm src I'm defined in App::Embra::Role::IncludeFromSrc
 
+=attr fragment
+
+The HTML fragment which links to the CSS file. Defaults to a C<link> element with C<rel="stylesheet">. Required by L<App::Embra::Role::Snippet>.
+
+=cut
+
 has 'fragment' => (
     is => 'lazy',
 );
 
 method _build_fragment { qq{<link rel="stylesheet" href="${ \ $self->href }" />} }
+
+=attr clipboard
+
+Where the L<C<fragment>|/fragment> should end up in files in your site. Defaults to 'head'. Required by L<App::Embra::Role::Snippet>.
+
+=cut
 
 has 'clipboard' => (
     is      => 'ro',
