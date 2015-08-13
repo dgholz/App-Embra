2c09daea4905249fcfff00c8401dc83110b77042
Daniel Holz <dgholz@gmail.com>
2015-08-12 22:35:43 -0700

    A::E::R::FileAssembler doc reword

--- lib/App/Embra/Role/FileAssembler.pm
+++ lib/App/Embra/Role/FileAssembler.pm
@@ -9,9 +9,7 @@ use Moo::Role;
 
 =head1 DESCRIPTION
 
-This role should be implemented by any plugin which alters a file's content into its final format. It requires plugins provide a C<assemble_files> method, which will be called late in the process of collating your site.
-
-Plugins which implement this role can access the site's files via the C<L<files|App::Embra/files>> attribute of its C<L<embra|App::Embra::Role::Plugin/embra>> attribute.
+This should be consumed by plugins who want their C<assemble_files> method called just before the site is published. That method should examine C<< $self->embra->files >> to find files which meet the criteria of the plugin, and alter them into their final publishable form.
 
 =cut
 
