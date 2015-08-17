a9eab5965c1d3f71e635ce06a0c6662930ca31ce
Daniel Holz <dgholz@gmail.com>
2015-08-17 09:11:04 -0700

    A::E::R::FileTransformer doc reword

--- lib/App/Embra/Role/FileTransformer.pm
+++ lib/App/Embra/Role/FileTransformer.pm
@@ -3,16 +3,13 @@ use warnings;
 
 package App::Embra::Role::FileTransformer;
 
-# ABSTRACT: something that transforms file content, but not into its publishable form
+# ABSTRACT: something that transforms file content from one format into another
 
-use Method::Signatures;
 use Moo::Role;
 
 =head1 DESCRIPTION
 
-This role should be implemented by any plugin which alters a file's content. It requires plugins provide a C<transform_files> method, which will be called early in the process of collating your site.
-
-Plugins which implement this role can access the site's files via the C<L<files|App::Embra/files>> attribute of its C<L<embra|App::Embra::Role::Plugin/embra>> attribute.
+This should be consumed by plugins who want their C<transform_files> method called before the files are published. That method should examine C<< $self->embra->files >> to find files which meet the criteria of the plugin, and tranform their content into a new form (which includes adding new files wut content derived from some matching file).
 
 =cut
 
