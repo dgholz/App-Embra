4d86a46d4d33083c69d351ab5bc2e1123a596c79
Daniel Holz <dgholz@gmail.com>
2015-08-30 00:43:19 -0700

    A::E::Util doc reword

--- lib/App/Embra/Util.pm
+++ lib/App/Embra/Util.pm
@@ -3,7 +3,7 @@ use warnings;
 
 package App::Embra::Util;
 
-# ABSTRACT: place for misc. code
+# ABSTRACT: App::Embra code without a home
 
 use String::RewritePrefix;
 use Method::Signatures;
@@ -16,7 +16,7 @@ use vars qw( @EXPORT_OK );
 
     App::Embra::Util::expand_config_package_name( $pkg_name )
 
-Returns C<$pkg_name> rewritten into a package name. If C<$pkg_name> starts with C<=>, it is treated as a package name and returned unmodified. Otherwise, a copy of it is prefixed with C<App::Embra::Plugin> (and then C<Bundle>, if it starts with C<@>) and returned.
+Returns the full package name of an L<App::Embra> plugin (or plugin bundle) that C<$pkg_name> refers to. If C<$pkg_name> starts with C<=>, it is treated as a verbatim name and returned unmodified. Otherwise, it is prefixed with C<App::Embra::Plugin> (and then C<Bundle>, if it starts with C<@>) and returned.
 
 Used by L<App::Embra::MVP::Assembler> and L<App::Embra::App::Command::listdeps>, to turn section names in config files into package names.
 
