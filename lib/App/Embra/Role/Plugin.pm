d3d2dc946327a0fbf1446ef47a6e4dc6e54d04ae
Daniel Holz <dgholz@gmail.com>
2015-08-30 22:32:01 -0700

    A::E::R::Plugin->name can be undef
    but A::E::R::Plugin->register_plugin will not pass through an undef name

--- lib/App/Embra/Role/Plugin.pm
+++ lib/App/Embra/Role/Plugin.pm
@@ -48,17 +48,6 @@ has 'name' => (
     default => method { ref $self },
 );
 
-method BUILDARGS( @args ) {
-    my %args = @args;
-    for my $attr ( qw< name > ) {
-        if( not defined $args{$attr} ) {
-            delete $args{$attr};
-        }
-    }
-
-    return \%args;
-}
-
 =method register_plugin
 
     App::Embra::Role::Plugin->register_plugin( $embra, $name, $payload );
@@ -68,7 +57,10 @@ This static method creates a new instance of the consuming class, plugs it into 
 =cut
 
 method register_plugin( $class:, App::Embra :$embra, :$name, HashRef :$payload = {} ) {
-    my $self = $class->new( embra => $embra, name => $name, %{ $payload } );
+    if( defined $name ) {
+        $payload->{name} = $name;
+    }
+    my $self = $class->new( embra => $embra, %{ $payload } );
     $self->logger->debugf( '%sregistered with %s', $self->log_prefix, $payload );
     $embra->add_plugin( $self );
     return $self;
