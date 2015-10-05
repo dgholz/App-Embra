23a9f456b8a4a01e1852e77e75537676ae2989b8
Daniel Holz <dgholz@gmail.com>
2015-10-05 09:17:53 -0700

    A::E::PB::WrapZillaPlugin collects unknown args and passes them to A::E::P::Zilla

--- lib/App/Embra/PluginBundle/WrapZillaPlugin.pm
+++ lib/App/Embra/PluginBundle/WrapZillaPlugin.pm
@@ -15,10 +15,23 @@ This bundle will add a L<Dist::Zilla> plugin to your site, and wrap it with C< L
 
 =cut
 
+=attr payload
+
+The options for C< L</plugin> >. This will collect any arguments not recognised by the constructor (i.e. C<embra>, C<name>, & C<plugin>), and pass them to the wrapped plugin's constructor (via L<App::Embra::Plugin::Zilla>). Defaults to an empty hashref.
+
+=cut
+
+has 'payload' => (
+    is => 'ro',
+    default => method { {} },
+);
+
 method BUILD( $unknown_ctor_args ) {
     if( $self->name eq ref $self ) {
         $self->warning('not given a Dist::Zilla plugin to wrap! Expect an error soon...');
     }
+    # ugh
+    @{ $self->payload }{keys %{ $unknown_ctor_args }} = values %{ $unknown_ctor_args };
 }
 
 func expand_dist_zilla_plugin_name( $plugin_name ) {
@@ -40,7 +53,8 @@ method add_dependency( @_ )  {
 
 method configure_bundle {
     $self->add_plugin( 'Zilla',
-        name => $self->name,
+        %{ $self->payload },
+        name    => $self->name,
     );
     $self->add_dependency( q{=}.expand_dist_zilla_plugin_name( $self->name ) );
 }
