08f452cab68ee425dee8c923565f6d84beb52546
Daniel George Holz <dgholz@gmail.com>
2017-02-22 00:03:44 +0000

    wrong implementation

--- lib/App/Embra/App/Command/init.pm
+++ lib/App/Embra/App/Command/init.pm
@@ -39,19 +39,6 @@ No new options; the standard L<embra options|fApp::Embra::App::Command/GLOBAL OP
 
 =cut
 
-{
-    my $dir;
- 
-    sub get_templates {
-        $dir ||= Path::Class::dir( @_ );
-        while (my $file = $dir->next) {
-            return $file if -f $file;
-        }
-        undef $dir;
-    }
-
-}
-
 sub guess_template_vars {
     my ( $cwd ) = @_;
 
@@ -66,20 +53,62 @@ sub guess_template_vars {
     };
 }
 
+sub generate_templates {
+    my ( $templates_dir ) = @_;
+    $templates_dirs = ( Path::Class::dir( $templates_dir ) );
+    return sub {
+        for my $template_dir ( @template_dirs ) }
+            while ( my $entry = $templates_dir->next ) {
+                if ( -d $entry ) { push @templates_dirs, $entry; }
+                return $entry if -f $file;
+            }
+        }
+    };
+}
+
+sub generate_skeleton {
+    my ( $template_gen ) = @_;
+    return sub {
+        while ( my $template_name = template_gen->() ) {
+            my $target_name = fileparse( $tt, ".tt" );
+            return $tt, $target_name;
+        }
+    };
+}
+
+sub prepare_skeleton {
+    my ( $template_name, $target_name ) = @_;
+    $target_name->parent->mkpath({error => \my $err});
+    if (@$err) {
+        my $reason_formatter = sub {my($f,$m)=@_;if($f ne q{}){$f=" $f";};join ': ',$f,$m;};
+        for my $reason ( map { $reason_formatter->(%$_) } @$err ) {
+            $self->embra->error("could not create directory$reason");
+        }
+        return;
+    }
+    my $template = $template_name->open('r') or do {
+        $self->embra->error("could not open template '$template_name': $!");
+        return;
+    }
+    my $target = $target_name->open('w+') or do {
+        $self->embra->error("could not create '$target_name': $!");
+        return;
+    }
+    return $template, $target;
+}
+
 sub execute {
     my ( $self, $opt, $arg ) = @_;
 
     my $templates_dir = File::ShareDir::module_dir(__PACKAGE__);
     my $t = Template->new( INCLUDE_PATH => $templates_dir );
-    my $cwd = Path::Class::dir->absolute;
     my $template_vars = guess_template_vars( $cwd );
+    my $skeleton_g = generate_skeleton( generate_templates( $templates_dir ) );
 
-    while ( my $tt = get_templates( $templates_dir ) ) {
-        open(my $emplate, q{<}, $tt) or $self->embra->error("could not open '$tt': $!");
-        my $target_file = $cwd->file( scalar fileparse( $tt->relative( $templates_dir ), ".tt" ) );
-        $target_file->parent->mkpath or $self->embra->error("could not create '".$target_file->parent."': $!");
-        my $target = $target_file->open('w+') or $self->embra->error("could not create '$target_file': $!");
-        $t->process($tt, $template_vars, $target) or $self->embra->error('could not write target config: '.$t->error);
+    while ( my ($template_name, $target_name) = skeleton_g->() ) {
+        if ( my ($template, $target) = prepare_skeleton( $template_name, $target_name ) {
+            $t->process($template, $template_vars, $target) or $self->embra->error("could not write '$target_name': ".$t->error);
+        }
     }
 }
 
